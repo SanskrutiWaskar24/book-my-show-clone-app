cb4e1340ed550000c891d44c605cafa222d19b8a
SanskrutiWaskar24 <sanskrutiwaskar.skn.comp@gmail.com>
2025-09-07 21:33:31 +0530

    tailwind needs lesser ver

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
+
 import './App.css';
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import {Router, Route} from 'react-router-dom';
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      {/* <Route path="/" element={<Homepage/>} />
+      <Route path="/movie/:id" element={<MoviePage/>} />
+      <Route path="/plays" element={<Plays/>} /> */}
+    </Router>
+    
   );
 }
 
