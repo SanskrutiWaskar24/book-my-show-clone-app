b0a507a573fb950ca7324655855075b5a4512cde
SanskrutiWaskar24 <sanskrutiwaskar.skn.comp@gmail.com>
2025-09-15 16:26:11 +0530

    day-06

---
+++ src/components/MovieHero/MovieInfo.Component.jsx
@@ -0,0 +1,41 @@
+import React, { useContext, useState } from 'react';
+import { MovieContext } from '../../context/MovieContext';
+
+const MovieInfo = ()=>{
+
+    const [isOpen, setIsOpen] = useState(false);
+    const [price, setPrice] =useState(0);
+
+    const {movie} = useContext(MovieContext);
+
+    const genres = movie.genres?.map(({name})=> name).join(", ");
+
+    const rentMovie = () => {
+        setIsOpen(true);
+        setPrice(149)
+    }
+    const buyMovie = () => {
+        setIsOpen(true);
+        setPrice(999)
+    }
+
+    return <>
+    {/* <PaymentModel setIsOpen={setIsOpen} setIsOpen={isOpen} price={price}/> */}
+    <div className='flex flex-col gap-8'>
+        <h1 className='text-white text-5xl font-bold'>
+            {movie.original_title}
+        </h1>
+        <div className="flex flex-col gap-2 text-white font-semibold">
+            <h4>4.4k rating</h4>
+            <h4>Hindi, English, Marathi, Tamil, Telugu</h4>
+            <h4>{movie.runtime}min | {genres}</h4>
+        </div>
+        <div className="flex items-center gap-3 text-xl px-4">
+            <button className="bg-red-500  py-3 text-white font-semibold rounded-lg">Rent $149</button>
+            <button className="bg-red-500 py-3 text-white font-semibold rounded-lg">Buy $999</button>
+        </div>
+    </div>
+    </>
+}
+
+export default MovieInfo;
--- src/pages/Movie.page.jsx
+++ src/pages/Movie.page.jsx
@@ -8,7 +8,8 @@ import { FaCcVisa, FaCcApplePay } from "react-icons/fa";
 import PosterSlider from "../components/PosterSlider/PosterSlider.Component";
 import MovieHero from "../components/MovieHero/MovieHeroComponent.jsx";
 import { MovieContext } from "../context/MovieContext.jsx";
-
+import { BiSlideshow } from "react-icons/bi";
+import Cast from "../components/Cast/Cast.Component.jsx"
 
 const MoviePage = () => {
 
@@ -52,12 +53,77 @@ const MoviePage = () => {
     requestMovie();
   }, [id]);
 
-  const settingsCast = {};
-  const settings = {};
+ const settingsCast = {
+    infinite: false,
+    speed: 500,
+    slidesToShow: 4,
+    slidesToScroll: 4,
+    initailSlide: 0,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 4,
+          slidesToScroll: 4,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 5,
+          slidesToScroll: 2,
+          initailSlide: 1,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          initailSlide: 4,
+        },
+      },
+    ],
+  };
+
+  const settings = {
+    infinite: false,
+    speed: 500,
+    slidesToShow: 4,
+    slidesToScroll: 4,
+    initailSlide: 0,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          initailSlide: 3,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+          initailSlide: 2,
+        },
+      },
+    ],
+  };
+  
 
 
   return <>
-  <MovieHero />
+  
+    <MovieHero />
     <div className="my-12 container px-4 lg-ml-20 lg:2/1">
       <div className="flex flex-col items-start gap-3">
         <h1 className="text-gray-800 font-bold gap-3 text-2xl">About the Movie</h1>
@@ -104,9 +170,7 @@ const MoviePage = () => {
           <div className="my-8">
             <hr />
           </div>
-
-            {/* cast sliders */}
-
+  {/* XCLUSIVE Movies */}
           <div className="my-8">
             <PosterSlider 
             config={settings} 
@@ -116,10 +180,28 @@ const MoviePage = () => {
             />
           </div>
 
+          {/* horizontal line */}
           <div className="my-8">
             <hr />
           </div>
 
+  {/* Cast Slider  */}
+          <div className="my-8">
+            <h2 className="text-gray-800 font-bold text-2xl mb-4">Cast and Crew</h2>
+            <Slider {...settingsCast}>
+              {cast.map((castData) => (
+                <Cast image={castData.profile_path} castName={movie.original_name} role={movie.character} />
+              ))}
+            </Slider>
+          </div>
+
+
+
+          {/* horizontal line */}
+          <div className="my-8">
+            <hr />
+          </div>
+  {/* Recommended Movies */}
           <PosterSlider 
           config={settings} 
           title="Recommended Movies"
@@ -127,7 +209,7 @@ const MoviePage = () => {
           isDark={false}
           />
     </div>
-  </>;
+  </>
 };
 
 export default MovieLayoutHoc(MoviePage);
